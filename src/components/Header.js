8de8cfc0dfddc200365c2694d55d6d7dcfadecb4
KirbyPatrick <pkirbyinatx@gmail.com>
2019-06-30 16:11:42 -0500

    added and stylized Header.js component to be the apps header menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
+import logo from "../logo.png";
 
 const Header = () => {
   return (
-    <div className="boxy">
+    <div className="headerContainer">
       <h3>Home</h3>
       <h3>About</h3>
       <h3>Contact</h3>
+      <div className="titleContainer">
+        <h1>Chuck Norris Facts</h1>
+      </div>
+      <div className="logoContainer">
+        <img src={logo} alt="Smiley face" height="75" width="75" />
+      </div>
     </div>
   );
 };
