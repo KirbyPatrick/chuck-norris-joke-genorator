af6ed471187daaece2a0a784d9c428daa90e4932
KirbyPatrick <pkirbyinatx@gmail.com>
2019-06-30 14:46:20 -0500

    created react app for project, removed react boilerplate, added skelatal header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="boxy">
+      <h3>Home</h3>
+      <h3>About</h3>
+      <h3>Contact</h3>
+    </div>
+  );
+};
+
+export default Header;
