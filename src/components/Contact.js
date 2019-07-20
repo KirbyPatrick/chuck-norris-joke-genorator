c019e95eedf99e0f1b0b6f27b0bde2e77964e76f
KirbyPatrick <pkirbyinatx@gmail.com>
2019-07-20 14:34:51 -0500

    filled out Contact.js by adding contact logos and linking them to correct paths

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
+import githubLogo from "../img/githubLogo.png";
+import linkedinLogo from "../img/linkedinLogo.png";
+import codewarsLogo from "../img/codewarsLogo.png";
+import twitterLogo from "../img/twitterLogo.png";
+import emailLogo from "../img/emailLogo.png";
+
+const flexContainer = {
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignContent: "center"
+};
+
+const imgStyles = {
+  width: 100,
+  height: 100,
+};
 
 const Contact = () => {
   return (
-    <div className='cardStyle'>
-      <h1>Contact Information:</h1>
+    <div className="cardStyle">
+      <div>
+        <h1>Contact Information:</h1>
+      </div>
+      <div style={flexContainer}>
+        <a href="https://github.com/KirbyPatrick" target="_blank">
+          <img style={imgStyles} src={githubLogo} alt="" />
+        </a>
+        <a href="https://www.linkedin.com/in/patrick-d-kirby/" target="_blank">
+          <img style={imgStyles} src={linkedinLogo} alt="" />
+        </a>
+        <a href="https://www.codewars.com/users/KirbyPatrick" target="_blank">
+          <img style={imgStyles} src={codewarsLogo} alt="" />
+        </a>
+        <a href="https://twitter.com/Kirby_Patrick" target="_blank">
+          <img style={imgStyles} src={twitterLogo} alt="" />
+        </a>
+        <a href="mailto:pKirbyInAtx@gmail.com">
+          <img style={imgStyles} src={emailLogo} alt="" />
+        </a>
+      </div>
     </div>
   );
 };
